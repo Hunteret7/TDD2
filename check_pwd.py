098917a962098daa2d7e072fe8a455b1512058a6
Hunteret7 <hunteret@oregonstate.edu>
2022-02-13 19:01:07 -0800

    proper lower case

--- check_pwd.py
+++ check_pwd.py
@@ -2,21 +2,21 @@ def check_pwd(val):
     has_spec = False
     size = False
     has_upper = False
+    has_lower = False
     spec_chars = "~`!@#$%^&*()_+-="
     for x in val:
         if x.isupper():
             has_upper = True
-            print("a")
+        if x.islower():
+            has_lower = True
         for y in spec_chars:
             if x == y:
                 has_spec = True
-                print("B")
     if val == "aaaaaaaa":
         return False
-    if len(val) > 7 and len(val) < 21:
+    if 7 < len(val) < 21:
         size = True
-        print("c")
-    if has_spec == False or size == False or has_upper == False:
+    if has_spec == False or size == False or has_upper == False or has_lower == False:
         return False
     else:
         return True
